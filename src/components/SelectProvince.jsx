372c0df1d09d011f3eb0286bd6eca8dfb1e1b2ea
nurofsun <dev.nurahmat@gmail.com>
2020-12-24 08:43:28 +0700

    Styled select element only with CSS

--- src/components/SelectProvince.jsx
+++ src/components/SelectProvince.jsx
@@ -1,12 +1,20 @@
-function SelectProvince({ onSelectProvinceChange, provinces }) {
+import styled from 'styled-components';
+
+const StyledSelect = styled.select`
+    appearance: none;
+    padding: 7px;
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 10px;
+    background-color: white;
+`;
+
+function SelectProvince({ onSelectProvinceChange, provinces, ...restProps }) {
     return (
-        <select 
-            style={{ width: '100%', padding: '10px', appearance: 'none', backgroundColor: 'white', border: 'none' }}
-            name="province-selector" onChange={onSelectProvinceChange}>
+        <StyledSelect onChange={onSelectProvinceChange} {...restProps}>
             {provinces.map((item,index) => (
                 <option key={item.provinsi} value={index}>{item.provinsi}</option>
             ))}
-        </select>
+        </StyledSelect>
     )
 }
 
